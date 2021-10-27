4849544342321d9305708e2314b7bc8889ae8f27
Usuario <husky.3125@gmail.com>
2021-10-27 11:51:55 +0200

    update

---
+++ EJERCICIO 6 CLASE.py
@@ -0,0 +1,7 @@
+vowels = ["a", "e", "i", "o","u"]
+word = input("Introduce la palabra: ")
+v_freg = {"a":0, "e":0, "i":0, "o":0, "u":0}
+for letter in word:
+    if letter in vowels:
+        v_freg[letter] += 1
+print("Frecuencia de las vocales en {word}:\n{v_freg}", word, v_freg)
